ec97aa01062c95373336389feef144b745ba304a
Yandr2022 <sharkievich@gmail.com>
2023-08-19 07:00:48 +0300

    first lombok example

--- src/main/java/by/Yandr2022/SBJavaCourse/Demo.java
+++ src/main/java/by/Yandr2022/SBJavaCourse/Demo.java
@@ -1,2 +1,13 @@
-package by.Yandr2022.SBJavaCourse;public class Demo {
+package by.Yandr2022.SBJavaCourse;
+
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Yuli", 100000,new Date());
+        System.out.println(employee);
+        employee.setName("Yandr2022");
+        System.out.println(employee.getName());
+        employee.setWorkStart(null);
+    }
 }
--- src/main/java/by/Yandr2022/SBJavaCourse/Employee.java
+++ src/main/java/by/Yandr2022/SBJavaCourse/Employee.java
@@ -1,2 +1,20 @@
-package by.Yandr2022.SBJavaCourse;public class Employee {
+package by.Yandr2022.SBJavaCourse;
+
+import lombok.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@ToString(exclude = "workStart")
+public class Employee {
+
+    private String name;
+    private Integer salary;
+    private Date workStart;
+
+
+
+
 }
